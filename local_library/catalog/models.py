9ce3d7f28a0d4ddacd8af2e2f02976ca6af9c8b7
hosecloud <4osecloud@gmail.com>
2023-03-16 17:30:43 +0900

    Add admin page

--- local_library/catalog/models.py
+++ local_library/catalog/models.py
@@ -32,6 +32,12 @@ class Book(models.Model):
 
     def get_absolute_url(self):
         return reverse('book-detail', args=[str(self.id)])
+    
+    def display_genre(self):
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
+
 
 class BookInstance(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across whole library')
