8878f85feb4b73a8fa9add916f4152e2a330bd2f
hosecloud <4osecloud@gmail.com>
2023-03-23 20:43:58 +0900

    Add author_list·detail

--- local_library/catalog/views.py
+++ local_library/catalog/views.py
@@ -35,3 +35,10 @@ class BookListView(generic.ListView):
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 2
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
